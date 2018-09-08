0c03cea6ca891d447947e7a027df89b731013cd2
Artur Koszewski <artur.koszewski@certusoft.pl>
2018-09-08 11:32:25 +0200

    [2.2]

---
+++ my-app/src/App.vue
@@ -0,0 +1,59 @@
+<!--10- 1. There's a <template> part -->
+<template>
+  <div id="app">
+    <h2>My awesome list</h2>
+    <ul>
+      <li v-for="p in products" :key="p.id">{{ p.name }}</li>
+    </ul>
+    <p v-if="!products.length">No products!</p>
+    <button v-on:click="removeLast()">Remove last item</button>
+    <br/>
+    <input v-model="product" placeholder="new item" />
+    <button v-on:click="addItemFromInput(product)" style="display: inline;">Add item from input</button>
+  </div>
+</template>
+
+<!--21- 2. A <script> part -->
+<script>
+import uuid from 'uuid/v4';
+// 4. Now App is not mounted itself, we're just creating a component (more on that later - hold your horses!)
+export default {
+  name: 'app',
+  //11/ 5. Data can no longer be just an object to prevent accidental shared state
+  data() {
+    return {
+      products: [{
+        id: 0,
+        name: 'Coffee'
+      }, {
+        id: 1,
+        name: 'Pizza'
+      }]
+    }
+  },
+  methods: {
+    removeLast() {
+      this.products.pop();
+    },
+
+    addItemFromInput(product){
+      //this.products.push({id: this.products.length > 0 ? this.products[(this.products.length - 1)].id+1: 0, name: product});
+      var newId = uuid();
+      console.log(newId);
+      this.products.push({id: uuid(), name: product});
+      console.log(this.products);
+    },
+  }
+}
+</script>
+
+<!--9- 3. And <style> part -->
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
